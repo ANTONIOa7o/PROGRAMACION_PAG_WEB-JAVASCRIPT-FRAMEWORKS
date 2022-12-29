a402461a2cada9f32255066801e25390c46e4910
@ndragogo´s formacion <78384046+ANTONIOa7o@users.noreply.github.com>
2022-12-29 11:01:06 +0100

    Add files via upload

---
+++ I]Introduccion_JavaScript/ejemplo3-Funciones-I/js/script.js
@@ -0,0 +1,30 @@
+/*
+                ===============
+                 LAS FUNCIONES
+                ==============
+
+        Entendidas y definidas como declaraciones de expresiones
+        funcionales
+        Consta de la palabra reservada a su llamada fuction
+        Sintaxis:
+        1. Nombre de la funcion
+        2.Lista de parametros de la misma(que la definen),entre parentesis
+        y separados por ,
+        3.Las declaraciones en general, tanto en js, como en otros entornos
+        estará acotada por {
+
+        }
+        
+*/
+var nombre= window.prompt("Introduce tu nombre");
+function saludar() {
+
+    swal('Hola ' + nombre)
+}
+/*
+Despdir con nombre y apellids
+
+*/
+function despedir(params) {
+    
+}
