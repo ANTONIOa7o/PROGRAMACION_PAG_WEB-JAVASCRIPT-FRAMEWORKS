b8c09560c0caf57ff1d01141c1c68d4b7dceeb28
@ndragogo´s formacion <78384046+ANTONIOa7o@users.noreply.github.com>
2023-01-03 10:06:36 +0100

    Add files via upload

---
+++ I]Introduccion_JavaScript/ejemplo4-Funciones-II/js/scriptPractica.js
@@ -0,0 +1,10 @@
+function miHora() {
+    var hora = new Date();
+    document.getElementById('resultado').innerHTML=
+    hora.getHours()+ ":" + hora.getMinutes() + ":" + hora.getSeconds();
+    
+}
+
+function cerrar() {
+    document.getElementById('resultado').innerHTML='';
+}
